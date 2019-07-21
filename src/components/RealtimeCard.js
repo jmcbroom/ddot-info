21810d9671df12a8fe980547e1332e3b816cc06e
Jimmy McBroom <jmcbroom@posteo.net>
2019-07-21 14:02:44 -0400

    RealtimeCard implemented

---
+++ src/components/RealtimeCard.js
@@ -0,0 +1,89 @@
+import React from 'react'
+import moment from 'moment'
+
+import { Link } from 'gatsby'
+import { Card, CardContent} from '@material-ui/core';
+import { DirectionsBus, SpeakerPhone, Schedule, Warning } from '@material-ui/icons';
+
+const styles = {
+  prediction: {
+    display: 'flex',
+    alignItems: 'center',
+    opacity: '0.6',
+    marginTop: '.5em'
+  },
+  predictionIcon: {
+    marginRight: '.25em',
+    width: '1em'
+  },
+  ahead: {
+    color: 'darkgreen',
+    fontWeight: 700,
+    paddingLeft: '.25em'
+  },
+  behind: {
+    color: 'darkred',
+    fontWeight: 700,
+    paddingLeft: '.25em'
+  }
+}
+
+const computeTimeAway = (prediction) => {
+  if (prediction.tripStatus.predicted) {
+    return Math.ceil((prediction.predictedArrivalTime - moment()) / 60000)
+  } else { 
+    return Math.ceil((prediction.scheduledArrivalTime - moment()) / 60000) 
+  }
+}
+
+const RealtimeCard = ({ prediction, references }) => {
+
+  let nextStopId = prediction.tripStatus.nextStop.slice(5,)
+  let nextStop = references.stops.filter(s => s.id === prediction.tripStatus.nextStop)[0]
+
+  return (
+    <Card elevation={0} style={{ minWidth: 320, maxHeight: 500, display: 'flex', flexWrap: 'wrap' }}>
+      <CardContent>
+        {prediction.tripStatus.activeTripId === prediction.tripId ?
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <DirectionsBus style={{ height: 20, width: 20, borderRadius: 9999, background: 'rgba(0,0,0,.75)', padding: 2.5, color: 'white' }} />
+            <div style={{ marginLeft: '.5em' }}>
+              {prediction.tripStatus.predicted ? `Arrives here` : `Scheduled to arrive`}
+              <span style={{ fontWeight: 700, paddingLeft: '.25em' }}>{computeTimeAway(prediction) > 0 ? `in ${computeTimeAway(prediction)} minutes`: `${Math.abs(computeTimeAway(prediction))} minutes ago`}</span>
+            </div>
+          </div>
+          : <div style={styles.prediction}><Warning style={styles.predictionIcon}/>This bus is still finishing another trip and will turn around before starting this trip</div>
+        }
+        <div style={{ marginTop: '.5em', display: 'flex', alignItems: 'center' }}>
+          <DirectionsBus style={{ height: 20, width: 20, borderRadius: 9999, background: 'rgba(0,0,0,.75)', padding: 2.5, color: 'white' }} />
+          <span style={{ marginLeft: '.5em' }}>Now near</span>
+          <Link 
+            style={{ fontSize: '1em', color: '#000', fontWeight: 300, padding: '0px 5px 0px 5px' }} 
+            to={`/stop/${nextStopId}/`}>
+            <span style={{fontWeight: 700}}>{nextStop.name}</span>
+          </Link>
+          <span style={{ background: '#eee', padding: '.25em', display: 'inline-block' }}>#{nextStopId}</span>
+        </div>
+        {prediction.tripStatus.predicted ? 
+          <div style={styles.prediction}>
+            <SpeakerPhone style={{ height: 20, width: 20, borderRadius: 9999, padding: 2.5, }}/>
+            <div style={{marginLeft: '.5em'}}>Real-time location 
+              {prediction.tripStatus.predicted ? 
+                (<span style={prediction.tripStatus.scheduleDeviation > 0 ? styles.behind : styles.ahead}>
+                    {prediction.tripStatus.scheduleDeviation === 0 ? `on time` : (
+                    `${Math.abs(prediction.tripStatus.scheduleDeviation/60)} min ${prediction.tripStatus.scheduleDeviation >= 0 ? ' late' : ' early'}`
+                    )}</span>)    
+              : `` }
+            </div>
+          </div>
+          : <div style={styles.prediction}>
+              <Schedule style={{ height: 20, width: 20, borderRadius: 9999, padding: 2.5, }}/>
+              <div style={{marginLeft: '.5em'}}>Scheduled location</div>
+            </div>
+        }
+      </CardContent>
+    </Card>
+  )
+}
+
+export default RealtimeCard;
--- src/components/RoutePredictionsList.js
+++ src/components/RoutePredictionsList.js
@@ -1,15 +1,15 @@
 import React, { useState, useEffect } from 'react';
 import moment from 'moment';
-import _ from 'lodash';
-import { Card, List, ListItem, ListItemIcon, ListItemText} from '@material-ui/core';
+import { Card, CardContent, List, ListItem, ListItemIcon, ListItemText, Collapse } from '@material-ui/core';
 import { Schedule, SpeakerPhone } from '@material-ui/icons';
 import Helpers from '../helpers';
+
+import RealtimeCard from './RealtimeCard'
 
 const RoutePredictionsList = ({ stop, trips, route, currentTrip, handleChange }) => {
 
   let [predictions, setPredictions] = useState([])
-
-  console.log(route)
+  let [references, setReferences] = useState([])
 
   useEffect(() => {
     const url = `${Helpers.endpoint}/arrivals-and-departures-for-stop/DDOT_${stop}.json?key=BETA&includePolylines=false`;
@@ -17,6 +17,7 @@ const RoutePredictionsList = ({ stop, trips, route, currentTrip, handleChange })
       .then(r => r.json())
       .then(d => {
 
+        console.log(d)
         let tripAdded = d.data.entry.arrivalsAndDepartures.map(dp => {
           let filteredTrip = trips.filter(tr => dp.tripId.indexOf(tr.tripId) > -1)[0]
           return {
@@ -24,8 +25,8 @@ const RoutePredictionsList = ({ stop, trips, route, currentTrip, handleChange })
             ...dp
           }
         })
-        console.log(tripAdded)
         setPredictions(tripAdded)
+        setReferences(d.data.references)
       });
   }, []);
 
@@ -35,34 +36,20 @@ const RoutePredictionsList = ({ stop, trips, route, currentTrip, handleChange })
     <Card style={{ margin:'0px 0px 10px 0px' }}>
       <List style={{ paddingTop: 0 }}>
         {matchedPredictions.length > 0 ? matchedPredictions.map((p, i) => (
-          <ListItem button key={p.tripId}>
+          <>
+          <ListItem button key={p.tripId} onClick={() => currentTrip === p.tripId ? handleChange(null) : handleChange(p.tripId)}>
             <ListItemIcon>
-              {p.predicted ? <Schedule /> : <SpeakerPhone /> }
+              {p.predicted ? <SpeakerPhone /> : <Schedule /> }
             </ListItemIcon>
             <ListItemText secondary={p.directionId} primary={`${p.predicted ? moment(p.predictedArrivalTime).format('h:mma') : moment(p.scheduledArrivalTime).format('h:mma')}`} />
           </ListItem>
-        )) : `No predictions`}
+          <Collapse in={currentTrip === p.tripId} timeout='auto' unmountOnExit>
+            <RealtimeCard prediction={p} references={references} />
+          </Collapse>
+          </>
+        )) : <Card><CardContent style={{ padding: '1em' }}>There are currently no real-time predictions available, see the schedule below.</CardContent></Card>}
 
-        
-        {/* {predictions.length > 0 ? Helpers.cleanPredictionHeadsign(predictions).map((p, i) => (
-          <div key={i}>
-            <ListItem button key={p.tripId} onClick={this.handleClick(p.tripId)} style={{ background: '#fff' }} >
-              <ListItemIcon>
-                {p.predicted ? <LiveSVG /> : <SchedSVG />}
-              </ListItemIcon>
-              <ListItemText style={{ fontWeight: this.state.open === p.tripId ? 700 : 300 }} secondary={_.capitalize(p.tripHeadsign)} primary={`${p.predicted ? moment(p.predictedArrivalTime).format('h:mma') : moment(p.scheduledArrivalTime).format('h:mma')}`} />
-              <ListItemIcon>
-                {this.state.open === p.tripId ? 
-                  <span style={{ display: 'flex', alignItems: 'center', fontSize: '.9em' }}>Showing live map<ExpandLess /></span> 
-                  : <span style={{ display: 'flex', alignItems: 'center', fontSize: '.9em' }}>{ i === 0 && !this.state.open ? `Where is this bus?` : null}<ExpandMore /></span>}
-              </ListItemIcon>
-            </ListItem>
-            <Collapse in={this.state.open === p.tripId && this.props.isOpen} style={{ marginBottom: '.5em' }} timeout="auto" unmountOnExit>
-              <RealtimeCard trip={p.tripId} stop={this.props.stop} status={p} route={this.props.route}/>
-            </Collapse>
-        </div>
-          )) : <Card><CardContent style={{ padding: '1em' }}>There are currently no real-time predictions available, see the schedule below.</CardContent></Card>} */}
-      </List>
+        </List>
     </Card>
   )
 }
